5e6694f3f69bfc1cf15b2fc80d374366daee2283
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-09-05 13:10:58 -0300

    Create 2339 - Aviões de Papel.c

---
+++ 2339 - Aviões de Papel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    int c, p, f;
+    
+    scanf("%d%d%d", &c, &p, &f);
+    
+    if(c * f > p)
+        printf("N\n");
+    else
+        printf("S\n");
+        
+    return 0;
+}
