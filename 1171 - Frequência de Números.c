74b9d020413b78e6ecd74095bfbdc1f42dea989f
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-09-06 11:06:02 -0300

    Update 1171 - Frequência de Números.c

--- 1171 - Frequência de Números.c
+++ 1171 - Frequência de Números.c
@@ -5,16 +5,16 @@
 int main(void){
    int num[TF] = {0}, i, n, m;
    
-   scanf("%d", &n);
+   scanf(" %d", &n);
    
    for(i = 0; i < n; i++){
-      scanf("%d", &m);
+      scanf(" %d", &m);
       num[m]++;
    }
    
    for (i = 1; i <= TF-10; i++){
       if(num[i] > 0)
-         printf("%d aparece %d vez (es)\n", i, num[i]);
+         printf("%d aparece %d vez(es)\n", i, num[i]);
    }
    
    return 0;
