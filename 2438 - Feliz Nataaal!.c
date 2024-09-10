8c65292c1585a02422c04b468f410b1f8111ce73
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-09-10 16:42:57 -0300

    Create 2438 - Feliz Nataaal!.c

---
+++ 2438 - Feliz Nataaal!.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main (void){
+    int l;
+    scanf("%d", &l);
+    printf("Feliz nat");
+    
+    for(int i = 0; i < l; i++)
+        printf("a");
+        
+    printf("l!\n");
+    
+    return 0;
+}
