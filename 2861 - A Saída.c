200a0778315ac32ab4aba7b453fb4ceb862d4e44
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-09-16 09:11:28 -0300

    Create 2861 - A Saída.c

---
+++ 2861 - A Saída.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+#define TF 1000
+
+int main (void){
+    int l;
+    char pergunta[TF];
+    
+    scanf("%d", &l);
+    for(int i = 0; i < l; i++){
+        gets(pergunta);
+        printf("gzuz\n");
+    }
+    
+    return 0;
+}
