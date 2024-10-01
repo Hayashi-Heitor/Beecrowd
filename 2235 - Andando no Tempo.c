4e16453f2b86d65efeea2c230547409fd812b4d9
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-10-01 10:59:15 -0300

    Create 2235 - Andando no Tempo.c

---
+++ 2235 - Andando no Tempo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a==b || b==c || c==a)
+        printf("S\n");
+    else if (a+b==c || a+c==b || b+c==a)
+        printf("S\n");
+    else
+        printf("N\n");
+
+    return 0;
+}
